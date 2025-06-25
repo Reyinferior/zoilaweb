a2619757e09a0a38ca00c9a5aceec99790aa91f2
Ángel Moreano <marioes0345678@gmail.com>
2025-06-25 14:35:30 -0500

    Primer commit: subiendo el proyecto a GitHub

---
+++ js/javaencabezado.js
@@ -0,0 +1,20 @@
+// Cargar header y footer dinámicamente
+document.addEventListener("DOMContentLoaded", () => {
+    const rutaBase = location.pathname.includes("/paginas/") ? "../" : "";
+    
+    includeHTML(rutaBase + "encabezado-piecera/header.html", "header-container");
+    includeHTML(rutaBase + "encabezado-piecera/footer.html", "footer-container");
+   
+});
+
+function includeHTML(file, elementId) {
+    fetch(file)
+        .then(response => {
+            if (!response.ok) throw new Error("Error al cargar " + file);
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
